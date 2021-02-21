a1be6d0d047b1862b4a88ad377a6f979f5e9b1c6
NIKHIL SINGH <nikhil.4598@gmail.com>
2021-02-21 22:11:51 +0530

    alert popup

--- web/main.js
+++ web/main.js
@@ -1,71 +1,90 @@
-const result = document.getElementById('result');
-const studentDetails = document.getElementById('studentDetails');
-const mailDetails = document.getElementById('mailDetails');
+const result = document.getElementById("result");
+const studentDetails = document.getElementById("studentDetails");
+const mailDetails = document.getElementById("mailDetails");
+const alertme = document.getElementById("alertpopup");
+
 
 const show = (element) => {
-    element.style.display = "flex";
-}
+  element.style.display = "flex";
+};
 
 const hide = (element) => {
-    element.style.display = "none";   
-}
+  element.style.display = "none";
+};
+const showalert = (alertme) => {
+  alertme.style.display = "flex";
+};
 
 const faceDetails = () => {
-    hide(studentDetails);
-    // eel.mainMenu(2, {
-    //     "id": document.getElementById('id').value,
-    //     "name": document.getElementById('name').value,
-    // })(function (response) {
-    //     alert(response);
-    // })
+  hide(studentDetails);
+  // eel.mainMenu(2, {
+  //     "id": document.getElementById('id').value,
+  //     "name": document.getElementById('name').value,
+  // })(function (response) {
+  //     alert(response);
+  // })
 
-    // show(elem);
-    // setTimeout(2000, hide(elem));
-    return false;
-}
+  // return false;
+};
 
 const sendMail = () => {
-    hide(mailDetails);
-    // eel.mainMenu(5, {
-    //     "receiver": document.getElementById('receiver').value,
-    // })(function (response) {
-    //     alert(response);
-    // })
-    return false;
-}
+  hide(mailDetails);
+  // eel.mainMenu(5, {
+  //     "receiver": document.getElementById('receiver').value,
+  // })(function (response) {
+  //     alert(response);
+  // })
+  //return false;
+};
 
 function checkCamera() {
-    // eel.mainMenu(1)(function (response) {
-    //     alert(response);
-    // });
+  //eel.mainMenu(1)(function (response) {
+  //     alert(response);
+
+  // });
+  showalert(alertme);
+  setTimeout(function () {
+    document.getElementById("alertpopup").style.display = "none";
+  }, 2000);
+  
 }
 
 function captureFaces() {
-    hide(mailDetails);
-    show(studentDetails);
+  hide(mailDetails);
+  show(studentDetails);
 }
 
 function trainImages() {
-    // eel.mainMenu(3)(function (response) {
-    //     alert(response);
-    // });
+  // eel.mainMenu(3)(function (response) {
+  //     alert(response);
+  // });
 }
 
 function recordAttendance() {
-    // eel.mainMenu(4)(function (response) {
-    //     alert(response);
-    // });
+  // eel.mainMenu(4)(function (response) {
+  //     alert(response);
+  // });
 }
 
 function mail() {
-    hide(studentDetails);
-    show(mailDetails);
+  hide(studentDetails);
+  show(mailDetails);
 }
 
-document.getElementById('btnn').addEventListener('click', () => show(studentDetails));
+document
+  .getElementById("btnn")
+  .addEventListener("click", () => show(studentDetails));
 
-document.getElementById('btnn-2').addEventListener('click', () => show(mailDetails));
+document
+  .getElementById("btnn-2")
+  .addEventListener("click", () => show(mailDetails));
 
-document.querySelector('.close').addEventListener("click", () => hide(studentDetails));
+document
+  .querySelector(".close")
+  .addEventListener("click", () => hide(studentDetails));
 
-document.querySelector('.close1').addEventListener("click", () => hide(mailDetails));
+document
+  .querySelector(".close1")
+  .addEventListener("click", () => hide(mailDetails));
+
+//document.getElementById('btnn3').addEventListener("click",()=> showalert(alertme));
