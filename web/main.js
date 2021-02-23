25b6998725928a07e7f5b6bd6c6e343c20341847
shaw8wit <shaw8wit@gmail.com>
2021-02-23 22:39:02 +0530

    adds web documentation

--- web/main.js
+++ web/main.js
@@ -1,23 +1,27 @@
+// some constants used throughout the project
 const result = document.getElementById("result");
-const studentDetails = document.getElementById("studentDetails");
-const mailDetails = document.getElementById("mailDetails");
 const alertme = document.getElementById("alertpopup");
+const mailDetails = document.getElementById("mailDetails");
+const studentDetails = document.getElementById("studentDetails");
 
-
+// makes element visible
 const show = (element) => {
   element.style.display = "flex";
 };
 
+// hides the element
 const hide = (element) => {
   element.style.display = "none";
 };
 
+// displays an alert
 const showAlert = (content) => {
   show(alertme);
   document.getElementById('modalpopup').innerHTML = content;
   setTimeout(() => hide(alertme), 3000);
 };
 
+// records face for given details
 const faceDetails = () => {
   hide(studentDetails);
   eel.mainMenu(2, {
@@ -27,6 +31,7 @@ const faceDetails = () => {
   return false;
 };
 
+// sends mail for given details
 const sendMail = () => {
   hide(mailDetails);
   eel.mainMenu(5, {
@@ -35,54 +40,58 @@ const sendMail = () => {
   return false;
 };
 
+// checks camera
 function checkCamera() {
   eel.mainMenu(1)(showAlert);
 }
 
+// displays form for details to capture face
 function captureFaces() {
   hide(mailDetails);
   show(studentDetails);
 }
 
+// trains images
 function trainImages() {
   eel.mainMenu(3)(showAlert);
 }
 
+// starts recording the attendance
 function recordAttendance() {
   eel.mainMenu(4)(showAlert);
 }
 
+// clears the form data
 function clear() {
   document.getElementById('id').value = "";
   document.getElementById('name').value = "";
   document.getElementById('email').value = "";
 }
 
+// displays form for details to send mail
 function mail() {
   hide(studentDetails);
   show(mailDetails);
 }
 
+// displays the respective forms on button click
 document
   .getElementById("btnn")
   .addEventListener("click", () => show(studentDetails));
-
 document
   .getElementById("btnn-2")
   .addEventListener("click", () => show(mailDetails));
 
+// hides and clears the respective forms on button clicks
 document
   .querySelector(".close")
   .addEventListener("click", () => {
     hide(studentDetails);
     clear();
   });
-
 document
   .querySelector(".close1")
   .addEventListener("click", () => {
     hide(mailDetails);
     clear();
   });
-
-//document.getElementById('btnn3').addEventListener("click",()=> showalert(alertme));
